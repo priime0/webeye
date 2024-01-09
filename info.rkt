7909b1d399ce03583e86f9bb6c349b153b488619
Lucas Sta Maria <lucas@priime.dev>
2024-01-09 13:30:01 -0500

    chore: create base project
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "webeye")
+(define deps '("base" "http-easy-lib"))
+(define pkg-desc "Observes webpages for changes, notifying if any occur")
+(define version "0.1")
+(define license 'MIT)
+(define pkg-authors '(priime0))

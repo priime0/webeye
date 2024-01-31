ae99625117bad51dc3c473fcde39b0c8c63e632a
Lucas Sta Maria <lucas@priime.dev>
2024-01-31 18:03:27 -0500

    chore: bump package version to 0.3
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define collection "webeye")
 (define deps '("base" "http-easy-lib"))
 (define pkg-desc "Observes webpages for changes, notifying if any occur")
-(define version "0.2")
+(define version "0.3")
 (define license 'MIT)
 (define pkg-authors '(priime0))
